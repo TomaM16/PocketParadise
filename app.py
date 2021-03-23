b643e3244488a30828f3186a6d1f424969eb1b7e
Toma Milkov <toma.milkov@gmail.com>
2021-03-23 22:18:33 +0200

    Done server for post requests from ESP32

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/my_plants', methods=['POST'])
+def my_plants():
+    json_file = request.get_json()
+    sensor = json_file['sensor']
+    print(sensor)
+
+    return 'OK'
 
 
 if __name__ == '__main__':
